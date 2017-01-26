93e58bff2e53931ab9ac82dc93d3ab6fb24a15ab
EC2 Default User <ec2-user@ip-172-31-31-225.us-west-2.compute.internal>
2017-01-26 21:28:44 +0000

    Make code style nicer

--- acodec.py
+++ acodec.py
@@ -3,23 +3,24 @@ import encodings, codecs
 
 # Our StreamReader
 class aStreamReader(codecs.StreamReader):
-    def readline(self, size=None, keepends=True):
-        if getattr(self, "pysrc", None)==None:          
-            r=self.stream.read().decode("utf8")
+    def readline(self, size = None, keepends = True):
+        if getattr(self, "pysrc", None) == None:          
+            r = self.stream.read().decode("utf8")
             ## ToDo ## r = outputFromInput(r)
-            self.pysrc=r.splitlines()
-        return  u'%s\n'%self.pysrc.pop(0) if self.pysrc else u''
+            self.pysrc = r.splitlines()
+        return  u'%s\n' % self.pysrc.pop(0) if self.pysrc else u''
         
 def search_function(s):
-    if s!="acodec": 
+    if s != "acodec": 
         return None
-    u8=encodings.search_function("utf8")
-    return codecs.CodecInfo( name='acodec', 
-        encode=u8.encode, decode=u8.decode,
-        incrementalencoder=u8.incrementalencoder,
-        incrementaldecoder=u8.incrementaldecoder,
-        streamreader=aStreamReader,        # acodec StreamReader
-        streamwriter=u8.streamwriter)
+    u8 = encodings.search_function("utf8")
+    return codecs.CodecInfo( name = 'acodec', 
+                             encode = u8.encode,
+                             decode = u8.decode,
+                             incrementalencoder = u8.incrementalencoder,
+                             incrementaldecoder = u8.incrementaldecoder,
+                             streamreader = aStreamReader,        # acodec StreamReader
+                             streamwriter = u8.streamwriter)
 
 codecs.register(search_function)  # register our new codec search function
 
