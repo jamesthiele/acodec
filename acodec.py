8c6c394518bde6112ae3d529a07d71c2bee5e497
James Thiele <james.thiele@gmail.com>
2017-02-09 12:38:18 -0800

    Make codec code more class oriented

--- acodec.py
+++ acodec.py
@@ -1,30 +1,32 @@
-# acodec.py
-import encodings, codecs
+import codecs, cStringIO, encodings
 
-# Our StreamReader
-class aStreamReader(codecs.StreamReader):
-    def outputFromInput(self, input):
+class StreamReader(codecs.StreamReader):
+    ## def __init__(self):
+        ## codecs.StreamReader.__init__(self, *args, **kwargs)
+        ## pass
+
+    def translate(self, input):
         output = input
         return output
 
     def readline(self, size = None, keepends = True):
         if getattr(self, "pysrc", None) == None:          
             input = self.stream.read().decode("utf8")
-            output = self.outputFromInput(input)
+            output = self.translate(input)
             self.pysrc = output.splitlines()
         return u'%s\n' % self.pysrc.pop(0) if self.pysrc else u''
         
 def search_function(s):
     if s != "acodec": 
         return None
-    u8 = encodings.search_function("utf8")
+    utf8 = encodings.search_function("utf8")
     return codecs.CodecInfo( name = 'acodec', 
-                             encode = u8.encode,
-                             decode = u8.decode,
-                             incrementalencoder = u8.incrementalencoder,
-                             incrementaldecoder = u8.incrementaldecoder,
-                             streamreader = aStreamReader,        # acodec StreamReader
-                             streamwriter = u8.streamwriter)
+                             encode = utf8.encode,
+                             decode = utf8.decode,
+                             incrementalencoder = utf8.incrementalencoder,
+                             incrementaldecoder = utf8.incrementaldecoder,
+                             streamreader = StreamReader,
+                             streamwriter = utf8.streamwriter)
 
-codecs.register(search_function)  # register our new codec search function
+codecs.register(search_function)
 
